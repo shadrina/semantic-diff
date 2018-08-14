e63af056f4079720a33787f05d018c53fd4a5ee5
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-08-14 15:18:19 +0300

    Stop removing relations if there is no partner node

--- src/main/kotlin/ru/nsu/diff/engine/matching/Postprocessor.kt
+++ src/main/kotlin/ru/nsu/diff/engine/matching/Postprocessor.kt
@@ -2,12 +2,14 @@ package ru.nsu.diff.engine.matching
 
 import com.intellij.util.text.EditDistance
 import ru.nsu.diff.util.BinaryRelation
+import ru.nsu.diff.util.ContextLevel
 import ru.nsu.diff.util.DeltaTreeElement
 import ru.nsu.diff.util.LongestCommonSubsequence
 
 class Postprocessor(private val relation: BinaryRelation<DeltaTreeElement>, private val t1Height: Int) : Matcher {
     fun match(node1: DeltaTreeElement) {
-        val node2 = relation.getPartner(node1) ?: return
+        val node2 = relation.getPartner(node1)
+                ?: return node1.children.forEach { match(it) }
 
         for (child in node1.children) {
             val partner = relation.getPartner(child)
@@ -22,14 +24,10 @@ class Postprocessor(private val relation: BinaryRelation<DeltaTreeElement>, priv
                 val candidateFromUnmatched = child findBestPartnerIn node2unmatchedChildren
                 val candidateFromMatched = child findBestPartnerIn node2matchedChildren
 
-                if (candidateFromUnmatched === null && candidateFromMatched === null) {
-                    continue
-
-                } else if (candidateFromUnmatched === null && candidateFromMatched !== null) {
+                if (candidateFromUnmatched === null && candidateFromMatched !== null) {
                     val realPartnerOfCandidate = relation.getPartner(candidateFromMatched)
-                    // ?
                     if (candidateFromMatched.betterPartnerFrom(child, realPartnerOfCandidate) === child
-                            || relation.getPartner(candidateFromMatched.parent!!) === node1)
+                            && relation.getPartner(candidateFromMatched.parent!!) === node1)
                         candidate = candidateFromMatched
 
                 } else if (candidateFromUnmatched !== null && candidateFromMatched === null) {
@@ -46,13 +44,9 @@ class Postprocessor(private val relation: BinaryRelation<DeltaTreeElement>, priv
                     }
                 }
 
-
             } else if (partner.parent !== node2) {
                 candidate = child findBestPartnerIn node2.children
 
-                // If on this stage there is no candidate, remove that and all
-                // underlying relations which led to that relation
-                if (candidate === null) child.removeRelationsInSubTree()
             }
             if (candidate !== null) {
                 relation.removePairWith(child)
@@ -63,14 +57,9 @@ class Postprocessor(private val relation: BinaryRelation<DeltaTreeElement>, priv
         }
     }
 
-    private fun DeltaTreeElement.removeRelationsInSubTree() {
-        relation.removePairWith(this)
-        children.forEach { it.removeRelationsInSubTree() }
-    }
-
     private infix fun DeltaTreeElement.findBestPartnerIn(nodes: List<DeltaTreeElement>) : DeltaTreeElement? {
         // TODO: Boundary percentage should depend on the height of the subtree
-        val boundaryPercentage = 1 - this.height() * 1.0 / t1Height
+        val boundaryPercentage = 1.0 * (1 - this.height() * 1.0 / t1Height)
         val strongBoundaryPercentage = 0.9
 
         var percentage = .0
@@ -110,6 +99,6 @@ class Postprocessor(private val relation: BinaryRelation<DeltaTreeElement>, priv
         val distance1 = EditDistance.levenshtein(text, node1.text, true)
         val distance2 = EditDistance.levenshtein(text, node2.text, true)
 
-        return if (distance1 < distance2) node1 else node2
+        return if (distance1 <= distance2) node1 else node2
     }
 }
