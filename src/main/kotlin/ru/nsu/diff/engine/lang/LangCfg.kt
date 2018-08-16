286473f633bbc6ed80d68797e42ee13b3b2d05bd
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-08-17 01:53:41 +0300

    Minor

--- src/main/kotlin/ru/nsu/diff/engine/lang/LangCfg.kt
+++
@@ -1,9 +0,0 @@
-package ru.nsu.diff.engine.lang
-
-abstract class LangCfg {
-    abstract val contextManager: ContextManager
-    abstract val uniqueInternalsNames: List<String>
-    abstract val fastMatcherEqualParameter: Double
-    abstract val similarityBoundaryCoefficient: Double
-    abstract val strongSimilarityBoundaryPercentage: Double
-}
